5100faec23f29ed7fb2d7820da29015b5dd83fe7
alonmorad9 <alon.morad@post.runi.ac.il>
2024-01-08 10:42:41 +0200

    first push

--- ArrayOps.java
+++ ArrayOps.java
@@ -17,8 +17,26 @@ public class ArrayOps {
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int n = array.length;
+        int maxV = 0;
+        int secondMax = 0;
+        for (int i = 0; i < n; i++) {
+            if (array[i] > maxV) {
+                maxV = array[i];
+            }
+        }
+        int [] newArr = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            if (array[i] != maxV) {
+                newArr[i] = array[i];
+            }
+        }
+        for (int i = 0; i < newArr.length; i++) {
+            if (newArr[i] > secondMax) {
+                secondMax = newArr[i];
+            }
+        }
+        return secondMax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
